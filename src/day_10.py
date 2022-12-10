1da36912bb71b053d451cf388f2ebdbba6ace649
Eugene Bikkinin <xnegxneg@gmail.com>
2022-12-10 12:26:04 +0100

    Small fix day 10.

--- src/day_10.py
+++ src/day_10.py
@@ -8,8 +8,7 @@ def noop(_, cycle, value):
     return cycle + 1, value, [value]
 
 def addx(match, cycle, value):
-    new_value = value + int(match.groups()[0])
-    return cycle + 2, new_value, [value, value]
+    return cycle + 2, value + int(match.groups()[0]), [value, value]
 
 ops = {r"noop": noop, r"addx (-?\d+)": addx}
 
@@ -25,7 +24,7 @@ for line in lines:
             cycle, x, addition = case(match, cycle, x)
             result.extend(addition)
 
-print(sum([result[point-1]*point for point in points_of_interest]))
+print(sum([result[point-1] * point for point in points_of_interest]))
 
 for line in chunker(result, 40):
     print(*['#' if abs(idx - register) < 2 else '.' for idx, register in enumerate(line)])
