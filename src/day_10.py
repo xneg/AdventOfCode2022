2a8114be24d5d8cb0093b2ef77aa3c9c5dc8678f
Eugene Bikkinin <xnegxneg@gmail.com>
2022-12-10 12:22:37 +0100

    Day 10.

---
+++ src/day_10.py
@@ -0,0 +1,31 @@
+import re
+
+from src.utils import chunker
+
+points_of_interest = [20, 60, 100, 140, 180, 220]
+
+def noop(_, cycle, value):
+    return cycle + 1, value, [value]
+
+def addx(match, cycle, value):
+    new_value = value + int(match.groups()[0])
+    return cycle + 2, new_value, [value, value]
+
+ops = {r"noop": noop, r"addx (-?\d+)": addx}
+
+lines = open("../inputs/day_10.txt", "r").read().splitlines()
+
+cycle = 0
+x = 1
+result = []
+for line in lines:
+    for pattern, case in ops.items():
+        match = re.compile(pattern).match(line)
+        if match:
+            cycle, x, addition = case(match, cycle, x)
+            result.extend(addition)
+
+print(sum([result[point-1]*point for point in points_of_interest]))
+
+for line in chunker(result, 40):
+    print(*['#' if abs(idx - register) < 2 else '.' for idx, register in enumerate(line)])
