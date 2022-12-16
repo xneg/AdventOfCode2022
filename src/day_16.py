df55559fe199646ee63edd570c2ef625a5c97f1f
Eugene Bikkinin <xnegxneg@gmail.com>
2022-12-16 19:14:00 +0100

    Day 16. First part. Very messy again.

--- src/day_16.py
+++ src/day_16.py
@@ -1,19 +1,78 @@
 import re
+from itertools import groupby
+
+from src.utils import printShortestDistance
+
 
 class Valve:
-    def __init__(self, name, pressure, others):
+    def __init__(self, name, pressure, neighbors):
         self.name = name
         self.pressure = int(pressure)
-        self.others = others.split(",")
+        self.neighbors = neighbors.split(", ")
+
+    def pressure_produce(self, start, end):
+        return (end - start) * self.pressure
 
 pattern = re.compile(r"Valve ([A-Z]{2}.*) has flow rate=(-?\d+); tunnels?\b leads?\b to valves?\b (.*)")
 
-lines = open("../inputs/day_16_test.txt", "r").read().splitlines()
+lines = open("../inputs/day_16.txt", "r").read().splitlines()
 
 valves = [Valve(*pattern.match(line).groups()) for line in lines]
+valves_dict = {valve.name: valve for valve in valves}
 
-for valve in valves:
-    print(valve.name, valve.pressure, valve.others)
+# prepare for using the shortest path
+valves_to_ints = {key: index for index, key in enumerate(valves_dict)}
+ints_to_valves = {index: key for index, key in enumerate(valves_dict)}
+graph_length = len(valves_dict)
+adj = [[] for i in range(graph_length)]
+for index, key in enumerate(valves_dict):
+    adj[index] = [valves_to_ints[n] for n in valves_dict[key].neighbors]
 
+valuable_valves = [valve.name for valve in valves_dict.values() if valve.pressure > 0]
 
+current = "AA"
+time_limit = 30
+current_time = 0
+opened_valves = []
 
+solution_space = []
+
+distances = {}
+for valve in valuable_valves:
+    distances[("AA", valve)], _ = printShortestDistance(adj, valves_to_ints["AA"], valves_to_ints[valve], graph_length)
+    distances[(valve, "AA")], _ = printShortestDistance(adj, valves_to_ints["AA"], valves_to_ints[valve], graph_length)
+    for other_valve in [v for v in valuable_valves if v != valve]:
+        distances[(valve, other_valve)], _ = printShortestDistance(adj, valves_to_ints[valve], valves_to_ints[other_valve], graph_length)
+
+def solve(current, available, current_time, path):
+    if len(available) == 0 or current_time == 30:
+        solution_space.append(path + [current])
+        return
+
+    for valve in available:
+        dist = distances[(current, valve)]
+        time = current_time + dist + 1
+        if time <= 30:
+            solve(valve, [a for a in available if a != valve], time, path + [current])
+
+    solution_space.append(path + [current])
+
+solve("AA", valuable_valves, 0, [])
+total = 0
+print(len(solution_space)) #86593
+for solution in solution_space:
+    result = 0
+    time = 0
+    for idx, point in enumerate(solution[1:]):
+        dist = distances[point, solution[idx]]
+        time = time + dist + 1
+        pressure = valves_dict[point].pressure_produce(time, time_limit)
+        result = result + valves_dict[point].pressure_produce(time, time_limit)
+    if result > total:
+        total = result
+print(total)
+exit()
+
+# 1572 ['AA', 'MD', 'DS', 'FS', 'HG', 'PZ', 'JE', 'YB', 'IT'] 34
+# 1574 ['AA', 'MD', 'DS', 'YW', 'FS', 'HG', 'PZ', 'JE', 'IK'] 30
+# 1580 ['AA', 'MD', 'DS', 'FS', 'HG', 'PZ', 'JE', 'YB'] 28
--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,83 @@ class PriorityQueue:
     def pop(self):
         value, priority = self.queue.pop()
         return value
+
+
+# utility function to form edge between two vertices
+# source and dest
+def add_edge(adj, src, dest):
+    adj[src].append(dest);
+    adj[dest].append(src);
+
+
+# a modified version of BFS that stores predecessor
+# of each vertex in array p
+# and its distance from source in array d
+def BFS(adj, src, dest, v, pred, dist):
+    # a queue to maintain queue of vertices whose
+    # adjacency list is to be scanned as per normal
+    # DFS algorithm
+    queue = []
+
+    # boolean array visited[] which stores the
+    # information whether ith vertex is reached
+    # at least once in the Breadth first search
+    visited = [False for i in range(v)];
+
+    # initially all vertices are unvisited
+    # so v[i] for all i is false
+    # and as no path is yet constructed
+    # dist[i] for all i set to infinity
+    for i in range(v):
+        dist[i] = 1000000
+        pred[i] = -1;
+
+    # now source is first to be visited and
+    # distance from source to itself should be 0
+    visited[src] = True;
+    dist[src] = 0;
+    queue.append(src);
+
+    # standard BFS algorithm
+    while (len(queue) != 0):
+        u = queue[0];
+        queue.pop(0);
+        for i in range(len(adj[u])):
+
+            if (visited[adj[u][i]] == False):
+                visited[adj[u][i]] = True;
+                dist[adj[u][i]] = dist[u] + 1;
+                pred[adj[u][i]] = u;
+                queue.append(adj[u][i]);
+
+                # We stop BFS when we find
+                # destination.
+                if (adj[u][i] == dest):
+                    return True;
+
+    return False;
+
+
+# utility function to print the shortest distance
+# between source vertex and destination vertex
+def printShortestDistance(adj, s, dest, v):
+    # predecessor[i] array stores predecessor of
+    # i and distance array stores distance of i
+    # from s
+    pred = [0 for i in range(v)]
+    dist = [0 for i in range(v)];
+
+    if (BFS(adj, s, dest, v, pred, dist) == False):
+        pass
+        # print("Given source and destination are not connected")
+
+    # vector path stores the shortest path
+    path = []
+    crawl = dest;
+    path.append(crawl);
+
+    while (pred[crawl] != -1):
+        path.append(pred[crawl]);
+        crawl = pred[crawl];
+
+    return dist[dest], [path[i] for i in range(len(path) - 1, -1, -1)]
